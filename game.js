6490aa3471d53242012eea8773bb1e4f53867b60
Xinyang <xinyzhang9@gmail.com>
2016-03-07 18:05:52 -0800

    start add damage

--- game.js
+++ game.js
@@ -1,57 +1,75 @@
 function playGround(){
 	var ch1 = new zhuge();
 	var ch2 = new wangsitu();
-
+	isPlay = true;
 	this.initialize = function(){
 		$(document).keydown(function(e){
-			if(e.keyCode == 81){ //q
+			if(e.keyCode == 81 && isPlay){ //q
 				document.getElementById('stage_gif').src = "assests/gif/action_10.gif";
-			}else if(e.keyCode == 87){ //w
+				ch2.health-=10;
+				console.log(isPlay)
+			}else if(e.keyCode == 87 && isPlay){ //w
 				document.getElementById('stage_gif').src = "assests/gif/action_11.gif";				
-			}else if(e.keyCode == 69){ //e
+			}else if(e.keyCode == 69 && isPlay){ //e
 				document.getElementById('stage_gif').src = "assests/gif/action_9.gif";				
-			}else if(e.keyCode == 65){ //a
+			}else if(e.keyCode == 65 && isPlay){ //a
 				document.getElementById('stage_gif').src = "assests/gif/action_8.gif";				
-			}else if(e.keyCode == 83){ //s
+			}else if(e.keyCode == 83 && isPlay){ //s
 				document.getElementById('stage_gif').src = "assests/gif/action_6.gif";				
-			}else if(e.keyCode == 68){ //d
-				document.getElementById('stage_gif').src = "assests/gif/action_14.gif";				
-			}else if(e.keyCode == 73){ //i
+			}else if(e.keyCode == 68 && isPlay){ //d
+				document.getElementById('stage_gif').src = "assests/gif/action_14.gif";
+
+
+			}else if(e.keyCode == 73 && isPlay){ //i
 				document.getElementById('stage_gif').src = "assests/gif/action_13.gif";				
-			}else if(e.keyCode == 79){ //o
+			}else if(e.keyCode == 79 && isPlay){ //o
 				document.getElementById('stage_gif').src = "assests/gif/action_7.gif";				
-			}else if(e.keyCode == 112){ //p
+			}else if(e.keyCode == 80 && isPlay){ //p
 				document.getElementById('stage_gif').src = "assests/gif/action_12.gif";				
-			}else if(e.keyCode == 74){ //j
+			}else if(e.keyCode == 74 && isPlay){ //j
 				document.getElementById('stage_gif').src = "assests/gif/action_1.gif";				
-			}else if(e.keyCode == 75){ //k
+			}else if(e.keyCode == 75 && isPlay){ //k
 				document.getElementById('stage_gif').src = "assests/gif/action_5.gif";				
-			}else if(e.keyCode == 76){ //l
+			}else if(e.keyCode == 76 && isPlay){ //l
 				document.getElementById('stage_gif').src = "assests/gif/action_2.gif";				
 			}
 		})
-	}
 
-	function zhuge(ch){
-		var health = 100;
-		var maxHealth = 100;
-		var canvas = document.getElementById('health1');
-		var context = canvas.getContext('2d');
-		var object1 = {
-		    x: 20,
-		    y: 30,
-		    width: 300,
-		    height: 20
-		};
-		// Loop
-		setInterval(onTimerTick, 33);
+}
+	this.mainLoop = function(){
+		ch1.check();
+		ch2.check();
+		if(!(ch1.isLive && ch2.isLive)){
+			isPlay = false;
+		}
+
+	}
+	this.checkplay = function(){
+		return isPlay;
+	}
+}
+
+function zhuge(){
+	this.health = 100;
+	var maxHealth = 100;
+	this.isLive = true;
+	var canvas = document.getElementById('health1');
+	canvas.width = 200
+	canvas.height = 70
+	var context = canvas.getContext('2d');
+	var object1 = {
+		x: 20,
+		y: 30,
+		width: 200,
+		height: 20
+	};
 		// Render Loop
-		function onTimerTick() {
+		this.check = function(){
 		    // Clear the canvas
 		    canvas.width = canvas.width;
 		    
 		    // Calculate health bar percent
-		    var percent = health / maxHealth;
+		    var percent = this.health / maxHealth;
 
 		    context.fillStyle = "Red";
 		    context.font = "18px sans-serif";
@@ -62,7 +80,52 @@ function playGround(){
 
 		    context.fillStyle = "red";
 		    context.fillRect(object1.x, object1.y, object1.width * percent, object1.height);
+		    if(this.health <=0){
+		    	this.isLive = false;
+		    	document.getElementById('stage_gif').src = "assests/img/zhuge_die.jpg";
+
+		    }
 		}
 
 	}
-}
+	function wangsitu(){
+		this.health = 100;
+		var maxHealth = 100;
+		this.isLive = true;
+		var canvas = document.getElementById('health2');
+		canvas.width = 200
+		canvas.height = 70
+		var context = canvas.getContext('2d');
+		var object1 = {
+			x: 20,
+			y: 30,
+			width: 200,
+			height: 20
+		};
+		// Render Loop
+		this.check = function(){
+		    // Clear the canvas
+		    canvas.width = canvas.width;
+		    
+		    // Calculate health bar percent
+		    var percent = this.health / maxHealth;
+
+		    context.fillStyle = "Red";
+		    context.font = "18px sans-serif";
+		    context.fillText("Life " + percent * 100 +"%", 20, 20);
+
+		    context.fillStyle = "black";
+		    context.fillRect(object1.x, object1.y, object1.width, object1.height);
+
+		    context.fillStyle = "red";
+		    context.fillRect(object1.x, object1.y, object1.width * percent, object1.height);
+
+		    if(this.health <=0){
+		    	this.isLive = false;
+		    	document.getElementById('stage_gif').src = "assests/gif/action_3.gif";
+
+		    }
+		}
+
+
+	}
